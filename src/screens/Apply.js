752260efee2d845f94a7d3cf6afb9a74e9daeee5
Prasang023 <prasang023@gmail.com>
2022-01-09 01:08:50 +0530

    accept

--- src/screens/Apply.js
+++ src/screens/Apply.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import tick from '../assets/images/tick.png'
+
+const appliedId = localStorage.getItem('appliedId')
 
 const Apply = () => {
+
+
     return (
-        <div>
-            
+        <div >
+            <img src={tick} />
+            <h3>Successfully Applied to borrow</h3>
+            <h5>You can contact the Admin using following</h5>
+            <p>phone number:</p>
+            <p>email</p>
         </div>
     )
 }
